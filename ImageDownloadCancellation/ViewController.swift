8c68141b593b84f9c478e7b233a40baa7f177a0a
Серебряков Артем <aserebryakov@avito.ru>
2017-01-16 12:08:29 +0300

    Initial Commit

---
+++ ImageDownloadCancellation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageDownloadCancellation
+//
+//  Created by Серебряков Артем on 16/01/2017.
+//  Copyright © 2017 seart13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
